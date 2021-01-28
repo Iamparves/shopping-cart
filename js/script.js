bd5642c384b7016a4d20f2f72e46a8a02c825a8e
Parves Hossain Rabby <parves.pe.wb.h@gmail.com>
2021-01-28 12:30:55 +0600

    final

---
+++ js/script.js
@@ -0,0 +1,39 @@
+document.getElementById('phone-increase').addEventListener('click', function () {
+    handleProductChange('phone', 1219, 1);
+})
+
+document.getElementById('phone-decrease').addEventListener('click', function () {
+    handleProductChange('phone', 1219, -1);
+})
+
+document.getElementById('case-increase').addEventListener('click', function () {
+    handleProductChange('case', 59, 1);
+})
+
+document.getElementById('case-decrease').addEventListener('click', function () {
+    handleProductChange('case', 59, -1);
+})
+
+function handleProductChange(product, unitPrice, add) {
+    const productInput = document.getElementById(product + '-count');
+    const productNewCount = parseInt(productInput.value) + add;
+    if (productNewCount <= 0) return
+    productInput.value = productNewCount;
+    const productTotal = productNewCount * unitPrice;
+    document.getElementById(product + '-total').innerText = productTotal;
+
+    calculateTotal()
+}
+
+function calculateTotal() {
+    const phoneTotal = parseFloat(document.getElementById('phone-total').innerText);
+    const caseTotal = parseFloat(document.getElementById('case-total').innerText);
+    const subtotal = document.getElementById('subtotal');
+    const total = document.getElementById('total');
+
+    subtotal.innerText = phoneTotal + caseTotal;
+    total.innerText = phoneTotal + caseTotal;
+}
+
+
+
